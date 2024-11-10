df24f93b06cdaf1d8610c62ad5f95612d037cad5
AlinkaKuznets <linaolen6@mail.ru>
2024-11-10 23:59:25 +0700

    Added Link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainPage from './components/pages/MainPage';
+import ShopCategory from './components/pages/ShopCategory';
+import Product from './components/pages/Product';
+import Cart from './components/pages/Cart'
+import LoginSignup from './components/pages/LoginSignup'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/пицца' element={<ShopCategory category='пицца' />} />
+          <Route path='/вок' element={<ShopCategory category='вок' />} />
+          <Route path='/шаурма' element={<ShopCategory category='шаурма' />} />
+          <Route path='/закуски' element={<ShopCategory category='закуски' />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import  './navbar.css'
+import React, { useState } from 'react'
+import './navbar.css'
 import { CiForkAndKnife } from "react-icons/ci";
 import { FaShoppingCart } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
+
+  const [menu, setMenu] = useState('новинки');
+
   return (
     <div className='navbar'>
       <div className='nav-logo'>
-      <CiForkAndKnife className='ci-1'/>
-      <h1>Печка</h1>
+        <CiForkAndKnife className='ci-1' />
+        <h1>Печка</h1>
       </div>
       <ul className='nav-menu'>
-        <li>Пиица <hr/></li>
-        <li>Вок</li>
-        <li>Шаурма</li>
-        <li>Закуски</li>
+        <li onClick={() => { setMenu('новинки') }}><Link style={{ textDecoration: 'none' }} to='/'>Новинки</Link>{menu === 'новинки' ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu('пицца') }}><Link style={{ textDecoration: 'none' }} to='/пицца'>Пиица</Link>{menu === 'пицца' ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu('вок') }}><Link style={{ textDecoration: 'none' }} to='/вок'>Вок</Link>{menu === 'вок' ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu('шаурма') }}><Link style={{ textDecoration: 'none' }} to='шаурма'>Шаурма</Link>{menu === 'шаурма' ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu('закуски') }}><Link style={{ textDecoration: 'none' }} to='закуски'>Закуски</Link>{menu === 'закуски' ? <hr /> : <></>}</li>
       </ul>
       <div className='nav-login-cart'>
-        <button>Войти</button>
-        <FaShoppingCart className='fa-1'/>
+        <Link to='/login'><button>Войти</button></Link>
+        <Link to='/cart'><FaShoppingCart className='fa-1' /></Link>
       </div>
     </div>
   )
